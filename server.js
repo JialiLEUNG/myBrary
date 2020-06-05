5a60dc31e25d28ab50b85d6db16d05a40bbd6d03
jialiLEUNG <liang.jiali.cs@gmail.com>
2020-06-05 14:43:49 -0700

    clean

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(express.static('public'))
   
   const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
+  mongoose.connect.set("useCreateIndex", true).connect(CONFIG.DB, { useUnifiedTopology: true,useNewUrlParser: true })
   const db = mongoose.connection
   db.on('error', error => console.error(error))
   db.once('open', () => console.log('Connected to Mongoose'))
